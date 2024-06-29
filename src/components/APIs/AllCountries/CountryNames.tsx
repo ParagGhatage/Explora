6db76b923b1396e2a63347f1100703c02877bf0c
ParagGhatage <phghatage1@gmail.com>
2024-06-29 21:45:14 +0530

    country names

--- src/components/APIs/AllCountries/CountryNames.tsx
+++ src/components/APIs/AllCountries/CountryNames.tsx
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export async function CountryQuery(){
+export async function CountryNames(){
     const response =await axios.get(`https://restcountries.com/v3.1/all?fields=name`)
     return(
         response.data
