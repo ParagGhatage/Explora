a5a342a64402fc972b5a577231c1303de4ec6c51
ParagGhatage <phghatage1@gmail.com>
2024-07-02 16:57:30 +0530

    Email Plan

--- src/app/PlanRoutes/page.tsx
+++ src/app/PlanRoutes/page.tsx
@@ -1,280 +1,211 @@
 'use client'
 import React, { useState, useEffect, ChangeEvent } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { PexelsQuery } from '@/components/APIs/Pexels/Query';
-import Image from 'next/image';
 import Navbar from '@/components/Navbar/Navbar';
-import { Run } from '@/components/APIs/Gemini/Recommendations';
 import { Plan } from '@/components/APIs/Gemini/RoutePlan';
-import { Divide } from 'lucide-react';
+import axios from 'axios';
 
-interface TouristDestinationsProps {
-  query: string;
-}
+const Route: React.FC = () => {
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [date, setDate] = useState('');
+  const [days, setDays] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [wantToSend, setWantToSend] = useState(false);
 
+  const [accommodation, setAccommodation] = useState({});
+  const [activities, setActivities] = useState([{}]);
+  const [budget, setBudget] = useState({});
+  const [packingList, setPackingList] = useState([]);
+  const [transportation, setTransportation] = useState([]);
 
+  const [plan, setTravelPlan] = useState<any>(null);
 
+  const handleStartChange = (e: ChangeEvent<HTMLInputElement>) => setStart(e.target.value);
+  const handleEndChange = (e: ChangeEvent<HTMLInputElement>) => setEnd(e.target.value);
+  const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => setDate(e.target.value);
+  const handleDaysChange = (e: ChangeEvent<HTMLInputElement>) => setDays(e.target.value);
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
 
-const Route: React.FC<TouristDestinationsProps> = () => {
-
-  //for input
-  const [start,setStart] = useState('')
-  const [end,setEnd] = useState('')
-  const [date,setDate] = useState("")
-  const [days,setDays] = useState()
-
-  //for overall travel plan
-  const [accommodation,setAccomodation] = useState({})
-  const [activities,setActivities] = useState([{}])
-  const [budget,setBudget] = useState({})
-  const [packingList,setPackingList] = useState([])
-  const [transportation,setTransportation] = useState([])
-
-  //for specific travel components
-  const [flight,setFlight] = useState({})
-  const [train,setTrain] = useState({})
-  const [road,setRoad] = useState({})
-
-  //Entire plan
-  const [plan,setTravelPlan] = useState({})
-  
-  const handleStartChange = (e: ChangeEvent<HTMLInputElement>) => {
-    
-    setStart(e.target.value);
-  };
-  const handleEndChange = (e: ChangeEvent<HTMLInputElement>) => {
-    
-    setEnd(e.target.value);
-  };
-  const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
-    
-    const dateString = e.target.value.toString();
-    console.log(dateString)
-    
-    setDate(dateString);
-  };
-  const handleDaysChange = (e: ChangeEvent<HTMLInputElement>) => {
-    
-    const days1 = e.target.value;
-    
-    setDays(days1);
-  };
-  
+  const onPlanSendClick = () => setWantToSend(true);
 
   const handleSearchSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = await Plan(start,end,date,days);
+    const data = await Plan(start, end, date, days);
     
-    console.log(data)
+    setAccommodation(data.Details.Accommodation[0]);
+    setActivities(data.Details.Activities);
+    setBudget(data.Details.Budget);
+    setPackingList(data.Details.PackingList);
+    setTransportation(data.Details.Transportation);
 
-    //Main fields
-    setAccomodation(data.Details.Accommodation[0])
-    setActivities(data.Details.Activities)
-    setBudget(data.Details.Budget)
-    setPackingList(data.Details.PackingList)
-    setTransportation(data.Details.Transportation)
-
-    //sub components
-    setFlight(data.Details.Transportation[0].Details)
-    setTrain(data.Details.Transportation[1].Details)
-    setRoad(data.Details.Transportation[2].Details)
-
-    //Entire plan
-    setTravelPlan({accommodation,activities,budget,packingList,transportation})
-
-    
+    setTravelPlan({ accommodation: data.Details.Accommodation[0], activities: data.Details.Activities, budget: data.Details.Budget, packingList: data.Details.PackingList, transportation: data.Details.Transportation });
   };
 
-
+  const onSendEmail = async () => {
+    try {
+      const response = await axios.post('/api/planSend', { accommodation, activities, budget, packingList, transportation, email, name });
+      console.log(response.data);
+    } catch (error: any) {
+      console.log('Unable to send email', error.message);
+    }
+  };
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200">
-  <Navbar />
-  <div className="max-w-screen mx-auto mt-16 p-6 bg-white rounded-lg shadow-lg">
-    <h1 className="text-5xl font-extrabold text-center text-gray-900 mb-8">Plan Your Trip</h1>
-    <form onSubmit={handleSearchSubmit} className="space-y-6">
-      <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0">
-        <input
-          type="text"
-          value={start}
-          onChange={handleStartChange}
-          placeholder="Your Location"
-          className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
-        />
-        <input
-          type="text"
-          value={end}
-          onChange={handleEndChange}
-          placeholder="Your Destination"
-          className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
-        />
-        <input
-          type="date"
-          value={date}
-          onChange={handleDateChange}
-          placeholder="Date of Departure"
-          className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
-        />
-        <input
-          value={days}
-          onChange={handleDaysChange}
-          placeholder="Days"
-          className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
-        />
+      <Navbar />
+      <div className="max-w-screen mx-auto mt-16 p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-5xl font-extrabold text-center text-gray-900 mb-8">Plan Your Trip</h1>
+        <form onSubmit={handleSearchSubmit} className="space-y-6">
+          <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0">
+            <input
+              type="text"
+              value={start}
+              onChange={handleStartChange}
+              placeholder="Your Location"
+              className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+            <input
+              type="text"
+              value={end}
+              onChange={handleEndChange}
+              placeholder="Your Destination"
+              className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+            <input
+              type="date"
+              value={date}
+              onChange={handleDateChange}
+              placeholder="Date of Departure"
+              className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+            <input
+              type="number"
+              value={days}
+              onChange={handleDaysChange}
+              placeholder="Days"
+              className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-4 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white font-bold rounded-lg shadow-lg hover:bg-gradient-to-l hover:from-fuchsia-500 hover:to-violet-500 transition-all duration-300"
+          >
+            Generate
+          </button>
+        </form>
       </div>
-      <button
-        type="submit"
-        className="w-full py-4 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white font-bold rounded-lg shadow-lg hover:bg-gradient-to-l hover:from-fuchsia-500 hover:to-violet-500 transition-all duration-300"
-      >
-        Search
-      </button>
-    </form>
-  </div>
 
-  <div className="max-w-screen mx-auto px-4 sm:px-6 lg:px-8 py-12">
-    {activities ? (
-      <div className="bg-white rounded-lg shadow-lg p-6 space-y-8">
-        <div className='text-center text-3xl font-semibold mb-6'>
-          Your plan &darr;
-        </div>
-        
-        {/* Transportation */}
-        <div>
-          <div className='text-black text-3xl font-bold text-center p-4 m-4'>
-            Transportation options
-          </div>
-          <div className='flex flex-wrap justify-center space-y-4 md:space-y-0 md:space-x-4'>
-            {/* Flight */}
-            <div className='p-6 bg-fuchsia-100 rounded-md text-center shadow-lg'>
-              <div className='text-center text-xl font-serif font-semibold mb-4'>
-                Flight
-              </div>
-              <ol className='space-y-2'>
-                <li>Info: {flight.AdditionalInfo}</li>
-                <li>Departure Location: {flight.DepartureLocation}</li>
-                <li>Departure Time: {flight.DepartureTime}</li>
-                <li>Duration: {flight.Duration}</li>
-                <li>Booking: {flight.Booking}</li>
-                <li>Arrival Location: {flight.ArrivalLocation}</li>
-                <li>Arrival Time: {flight.ArrivalTime}</li>
-                <li>Cost: {flight.Cost} {budget.Currency}</li>
-              </ol>
+      <div className="max-w-screen mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        {plan ? (
+          <div className="bg-white rounded-lg shadow-lg p-6 space-y-8">
+            <div className='text-center text-3xl font-semibold mb-6'>
+              Your plan &darr;
             </div>
-            
-            {/* Train */}
-            <div className='p-6 bg-fuchsia-100 rounded-md text-center shadow-lg'>
-              <div className='text-center text-xl font-serif font-semibold mb-4'>
-                Train
+            <div>
+              <button
+                type="button"
+                onClick={onPlanSendClick}
+                className="w-full py-4 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white font-bold rounded-lg shadow-lg hover:bg-gradient-to-l hover:from-fuchsia-500 hover:to-violet-500 transition-all duration-300"
+              >
+                Lets Go &rarr;
+              </button>
+              <div>
+                {wantToSend ? (
+                  <div>
+                    <input
+                      type="text"
+                      value={name}
+                      onChange={handleNameChange}
+                      placeholder="Your Name"
+                      className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+                    />
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      placeholder="Your Email address"
+                      className="flex-1 bg-teal-50 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-600"
+                    />
+                    <button
+                      type="button"
+                      onClick={onSendEmail}
+                      className="w-full py-4 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white font-bold rounded-lg shadow-lg hover:bg-gradient-to-l hover:from-fuchsia-500 hover:to-violet-500 transition-all duration-300"
+                    >
+                      Send &rarr;
+                    </button>
+                  </div>
+                ) : null}
               </div>
-              <ol className='space-y-2'>
-                <li>Info: {train.AdditionalInfo}</li>
-                <li>Departure Location: {train.DepartureLocation}</li>
-                <li>Departure Time: {train.DepartureTime}</li>
-                <li>Duration: {train.Duration}</li>
-                <li>Booking: {train.Booking}</li>
-                <li>Arrival Location: {train.ArrivalLocation}</li>
-                <li>Arrival Time: {train.ArrivalTime}</li>
-                <li>Cost: {train.Cost} {budget.Currency}</li>
-              </ol>
             </div>
-            
-            {/* Road */}
-            <div className='p-6 bg-fuchsia-100 rounded-md text-center shadow-lg'>
-              <div className='text-center text-xl font-serif font-semibold mb-4'>
-                Road
-              </div>
-              <ol className='space-y-2'>
-                <li>Info: {road.AdditionalInfo}</li>
-                <li>Departure Location: {road.DepartureLocation}</li>
-                <li>Departure Time: {road.DepartureTime}</li>
-                <li>Duration: {road.Duration}</li>
-                <li>Booking: {road.Booking}</li>
-                <li>Arrival Location: {road.ArrivalLocation}</li>
-                <li>Arrival Time: {road.ArrivalTime}</li>
-                <li>Cost: {road.Cost} {budget.Currency}</li>
-              </ol>
+
+            {/* Rendering the plan details */}
+            <div className="mt-8">
+              <h2 className="text-2xl font-bold text-gray-800 mb-4">Accommodation</h2>
+              <p>Check In Date: {accommodation.CheckInDate}</p>
+              <p>Check Out Date: {accommodation.CheckOutDate}</p>
+              <p>Info: {accommodation.AdditionalInfo}</p>
+              <p>Name of place: {accommodation.Name}</p>
+              <p>Address: {accommodation.Address}</p>
+              <p>Type: {accommodation.Type}</p>
+              <p>Contact info: {accommodation.ContactInfo}</p>
+              <p>Cost Per Night: {accommodation.CostPerNight}</p>
+              <p>Total Stay Cost: {accommodation.TotalCost}</p>
+
+              <h2 className="text-2xl font-bold text-gray-800 mb-4 mt-8">Transportation</h2>
+              {transportation.map((trans, index) => (
+                <div key={index} className="mb-4">
+                  <h3 className="text-xl font-semibold">{trans.Mode}</h3>
+                  <p>Info: {trans.Details.AdditionalInfo}</p>
+                  <p>Departure Location: {trans.Details.DepartureLocation}</p>
+                  <p>Departure Time: {trans.Details.DepartureTime}</p>
+                  <p>Duration: {trans.Details.Duration}</p>
+                  <p>Booking: {trans.Details.Booking}</p>
+                  <p>Arrival Location: {trans.Details.ArrivalLocation}</p>
+                  <p>Arrival Time: {trans.Details.ArrivalTime}</p>
+                  <p>Cost: {trans.Details.Cost} {budget.Currency}</p>
+                </div>
+              ))}
+
+              <h2 className="text-2xl font-bold text-gray-800 mb-4 mt-8">Budget</h2>
+              <p>Currency: {budget.Currency}</p>
+              <p>Total trip Cost: {budget.TotalCost}</p>
+              <p>Flight Cost: {budget.FlightCost}</p>
+              <p>Accommodation Cost: {budget.AccommodationCost}</p>
+              <p>Food Cost: {budget.FoodCost}</p>
+              <p>Activities Cost: {budget.ActivitiesCost}</p>
+
+              <h2 className="text-2xl font-bold text-gray-800 mb-4 mt-8">Activities</h2>
+              {activities.map((activity, index) => (
+                <div key={index} className="mb-4">
+                  <h3 className="text-xl font-semibold">{activity.Name}</h3>
+                  <p>Info: {activity.AdditionalInfo}</p>
+                  <p>Booking: {activity.Booking}</p>
+                  <p>Cost: {activity.Cost} {budget.Currency}</p>
+                  <p>Start Time: {activity.StartTime}</p>
+                  <p>End Time: {activity.EndTime}</p>
+                  <p>Location: {activity.Location}</p>
+                  <p>Type: {activity.Type}</p>
+                </div>
+              ))}
+
+              <h2 className="text-2xl font-bold text-gray-800 mb-4 mt-8">Packing List</h2>
+              <ul className="list-disc pl-5">
+                {packingList.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
             </div>
           </div>
-        </div>
-
-        {/* Activities */}
-        <div className='text-center'>
-          <div className='text-black text-3xl font-bold p-4 m-4'>
-            Activities
+        ) : (
+          <div className="text-black-700 text-center text-xl font-semibold">
+            Creating your trip plan...
           </div>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {activities.map((activity) => (
-              <div className='p-4 bg-fuchsia-100 rounded-md text-center shadow-lg'>
-                <ol className='space-y-2'>
-                  <li>Date: {activity.Date}</li>
-                  <li>Name: {activity.Name}</li>
-                  <li>Location: {activity.Location}</li>
-                  <li>Time: {activity.Time}</li>
-                  <li>Info: {activity.AdditionalInfo}</li>
-                  <li>Booking: {activity.Booking}</li>
-                  <li>Cost: {activity.Cost}</li>
-                </ol>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Accommodation */}
-        <div className='text-center bg-lime-200 rounded-md shadow-lg p-6'>
-          <div className='text-3xl p-4 font-sans font-bold'>
-            Accommodations
-          </div>
-          <ol className='space-y-2'>
-            <li>Check In Date: {accommodation.CheckInDate}</li>
-            <li>Check Out Date: {accommodation.CheckOutDate}</li>
-            <li>Info: {accommodation.AdditionalInfo}</li>
-            <li>Name of place: {accommodation.Name}</li>
-            <li>Address: {accommodation.Address}</li>
-            <li>Type: {accommodation.Type}</li>
-            <li>Contact info: {accommodation.ContactInfo}</li>
-            <li>Cost Per Night: {accommodation.CostPerNight}</li>
-            <li>Total Stay Cost: {accommodation.TotalCost}</li>
-          </ol>
-        </div>
-
-        {/* Expenses */}
-        <div className='text-center bg-lime-200 rounded-md shadow-lg p-6'>
-          <div className='text-3xl p-4 font-sans font-bold'>
-            Expenses
-          </div>
-          <ol className='space-y-2'>
-            <li>Currency: {budget.Currency}</li>
-            <li>Total trip Cost: {budget.TotalCost}</li>
-            <li>Flight Cost: {budget.FlightCost}</li>
-            <li>Accommodation Cost: {budget.AccommodationCost}</li>
-            <li>Food Cost: {budget.FoodCost}</li>
-            <li>Activities Cost: {budget.ActivitiesCost}</li>
-          </ol>
-        </div>
-
-        {/* Packing List */}
-        <div className='text-center bg-lime-200 rounded-md shadow-lg p-6'>
-          <div className='text-3xl p-4 font-sans font-bold'>
-            Packing List
-          </div>
-          <div className='space-y-2'>
-            {packingList.map((item) => (
-              <div className='p-2 m-1'>
-                {item}
-              </div>
-            ))}
-          </div>
-        </div>
+        )}
       </div>
-    ) : (
-      <div className="text-black-700 text-center text-xl font-semibold">
-        Creating your trip plan...
-      </div>
-    )}
-  </div>
-</div>
-
-
-)}
+    </div>
+  );
+}
 
 export default Route;
