39a76464d6f782251245aa77f28ba8f8e3b302d7
ParagGhatage <phghatage1@gmail.com>
2024-07-07 19:00:37 +0530

    page changes

--- src/app/Countries/CountryPhotos/TopDestinations/page.tsx
+++ src/app/Countries/CountryPhotos/TopDestinations/page.tsx
@@ -6,9 +6,6 @@ import Image from 'next/image';
 import Navbar from '@/Navbar/Navbar';
 import { Run } from '@/components/APIs/Gemini/Recommendations';
 
-interface TouristDestinationsProps {
-  query: string;
-}
 interface Photo {
   id: number;
   src: {
@@ -21,7 +18,7 @@ interface Photo {
 const TouristDestinations = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [destinations, setDestinations] = useState("");
-  const [Recommendations, setRecommendations] = useState();
+  const [Recommendations, setRecommendations] = useState<string[] | undefined>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -30,113 +27,106 @@ const TouristDestinations = () => {
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
+
   useEffect(() => {
     const fetchData = async () => {
       if (query) {
         try {
           const Recommendation: any = await Run(query);
           console.log(Recommendation)
-          const arr=Recommendation.places;
+          const arr = Recommendation.places;
 
           setRecommendations(arr);
         } catch (error) {
           console.error("Error fetching recommendations:", error);
         }
       }
-      
     };
     
     fetchData();
     
-  },[photos]);
+  }, [query]);
 
   const handleSearchSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = await PexelsQuery(searchQuery);
     setPhotos(data.photos);
-    setRecommendations([])
+    setRecommendations([]);
   };
-
-  
 
   return (
     <div className="min-h-screen bg-gradient-to-r pt-20 from-indigo-200 via-purple-200 to-pink-200">
   
-  <div className="max-w-7xl mx-auto mt-8 p-4">
-    <h1 className="text-4xl font-bold text-center text-gray-900 pb-4">Explore Travel Destinations</h1>
-    <form onSubmit={handleSearchSubmit} className="bg-white rounded-lg shadow-lg p-6">
-      <div className="flex items-center space-x-4">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleSearchChange}
-          placeholder="Search for travel destinations..."
-          className="bg-teal-50 p-4 w-full border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
-        />
-        <button
-          type="submit"
-          className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 p-5 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-    <div className='text-center m-5 w-100% '>
-        <a href="/PlanRoutes">
-    <button
-          
-          className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 p-5 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
-        >
-          Plan Your Trip &rarr;
-        </button>
-        </a>
-
-    </div>
-    
-
-    <div className="py-5">
-      {Recommendations ? (
-        <div>
-          
-          <div className='text-center text-3xl p-3'>
-            Suggestions
+      <div className="max-w-7xl mx-auto mt-8 p-4">
+        <h1 className="text-4xl font-bold text-center text-gray-900 pb-4">Explore Travel Destinations</h1>
+        <form onSubmit={handleSearchSubmit} className="bg-white rounded-lg shadow-lg p-6">
+          <div className="flex items-center space-x-4">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              placeholder="Search for travel destinations..."
+              className="bg-teal-50 p-4 w-full border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+            />
+            <button
+              type="submit"
+              className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 p-5 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
+            >
+              Search
+            </button>
           </div>
+        </form>
+        <div className='text-center m-5 w-100% '>
+          <a href="/PlanRoutes">
+            <button
+              className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 p-5 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
+            >
+              Plan Your Trip &rarr;
+            </button>
+          </a>
+        </div>
 
-          <div className="text-gray-700 flex flex-wrap justify-center">
-            {Recommendations.map((place) => (
-              <div key={place} className="p-3 text-left">
-                {place}
+        <div className="py-5">
+          {Recommendations ? (
+            <div>
+              <div className='text-center text-3xl p-3'>
+                Suggestions
               </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div className="text-gray-700 text-center">Loading recommendations...</div>
-      )}
-    </div>
 
-    <div className="container mx-auto py-8">
-      {photos.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {photos.map((photo) => (
-            <div key={photo.id} className="relative group">
-              <Image
-                src={photo.src.large2x}
-                alt="Travel destination"
-                height={500}
-                width={500}
-                className="object-cover rounded-lg shadow-lg transition-transform transform group-hover:scale-105"
-              />
+              <div className="text-gray-700 flex flex-wrap justify-center">
+                {Recommendations.map((place) => (
+                  <div key={place} className="p-3 text-left">
+                    {place}
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
+          ) : (
+            <div className="text-gray-700 text-center">Loading recommendations...</div>
+          )}
         </div>
-      ) : (
-        <p className="text-center text-gray-700 mt-10">No photos found for the query.</p>
-      )}
-    </div>
-  </div>
-</div>
 
+        <div className="container mx-auto py-8">
+          {photos.length > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              {photos.map((photo) => (
+                <div key={photo.id} className="relative group">
+                  <Image
+                    src={photo.src.original}
+                    alt="Travel destination"
+                    height={500}
+                    width={500}
+                    className="object-cover rounded-lg shadow-lg transition-transform transform group-hover:scale-105"
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className="text-center text-gray-700 mt-10">No photos found for the query.</p>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
