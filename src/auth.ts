72eed6fc678c88b18d7658168a00118f2302525b
ParagGhatage <phghatage1@gmail.com>
2024-07-07 20:21:59 +0530

    check

--- src/auth.ts
+++ src/auth.ts
@@ -1,37 +1,45 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { hashSync } from "bcryptjs";
+import bcrypt,{ hashSync} from "bcryptjs";
 import dbConnect from "./lib/dbConnect";
 import UserModel, { User } from "./models/user.model";
 
+// Use the defined interface in your NextAuth configuration
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       credentials: {
-        email: { type: "text" }, // Specify type as "text" for email
-        password: { type: "password" }, // Specify type as "password" for password
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
-      authorize: async (credentials: Partial<Record<"email" | "password", string>>, request: any) => {
+      async authorize(credentials: any): Promise<any> {
         dbConnect(); // Ensure db connection is established
+        let user: User | null = null;
 
         try {
-          const hashedPassword = hashSync(credentials.password || "", 10);
-
-          const user = await UserModel.findOne({
-            $or: [
-              { email: credentials.email || "" },
-              { password: hashedPassword },
-            ],
+          
+          user = await UserModel.findOne({
+            email: credentials.email
           });
 
           if (!user) {
             return null; // Return null if no user found
           }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error('Incorrect password');
+          }
 
-          return user as User; // Return the user object if found
+          // Return the user object if found
         } catch (error: any) {
           throw new Error(error.message || "An error occurred"); // Ensure error.message is a string or default to a generic message
         }
+        
       },
     }),
   ],
