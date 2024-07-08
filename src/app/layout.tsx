37a335db5eedbec8e5af3ec94688c1bf9a6f4214
ParagGhatage <phghatage1@gmail.com>
2024-07-08 16:18:45 +0530

    Navbar optional rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import type { Metadata } from "next";
+"use client"
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { usePathname } from "next/navigation"; // Import usePathname
 import Navbar from "@/Navbar/Navbar";
-
+import { metadata } from "./metadata"; // Import metadata
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Voyage Vista",
-  description: "Travel explorer and planner",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname(); // Use usePathname to get the current path
+  const noNavbarRoutes = ["/"];
+
   return (
     <html lang="en">
-      <SessionProvider>
-        <body className={inter.className}>
-          <Navbar/>
-          <div >{children}</div>
-        </body>
-      </SessionProvider>
+      <head>
+        <title>{metadata.title as string}</title>
+        <meta name="description" content={metadata.description as string} />
+      </head>
+      <body className={inter.className}>
+        <SessionProvider>
+          {!noNavbarRoutes.includes(pathname) && <Navbar />} {/* Conditionally render Navbar */}
+          <div>{children}</div>
+        </SessionProvider>
+      </body>
     </html>
   );
 }
