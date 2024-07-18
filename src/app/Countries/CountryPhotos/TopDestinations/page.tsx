5d2f9d50ea3465e42c5ec69acffb8843abb109ea
ParagGhatage <phghatage1@gmail.com>
2024-07-18 06:55:48 +0530

    arrange

--- src/app/Countries/CountryPhotos/TopDestinations/page.tsx
+++ src/app/Countries/CountryPhotos/TopDestinations/page.tsx
@@ -93,7 +93,7 @@ const TouristDestinations = () => {
                 Suggestions
               </div>
 
-              <div className="text-gray-700 flex flex-wrap justify-center sm:grid-cols-2 sm:justify-between">
+              <div className="text-gray-700 flex  justify-center sm:grid-cols-2 sm:justify-between">
                 {Recommendations.map((place, index) => (
                   <div key={index} className="p-3 text-left">
                     {place}
