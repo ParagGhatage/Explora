39deae95deac26150eb72066502f79da525c8611
ParagGhatage <phghatage1@gmail.com>
2024-07-20 22:35:49 +0530

    https

--- src/components/Weather/CreateCityId.tsx
+++ src/components/Weather/CreateCityId.tsx
@@ -7,7 +7,7 @@ export const Id = async(Lat:number =0,Lon:number=0) => {
 
     const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY
 
-    const CityData:any = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${Lat}&lon=${Lon}&limit=1&appid=${key}`)
+    const CityData:any = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${Lat}&lon=${Lon}&limit=1&appid=${key}`)
     const Cityname = CityData.data[0].name
     console.log(CityData.data[0].name)
 
