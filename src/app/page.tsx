4730a8c92b627f086f28f2f9d669d822420c508b
ParagGhatage <phghatage1@gmail.com>
2024-07-22 05:22:00 +0530

    Landing image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+"use client"
 import Image from "next/image";
 import { Landing } from "@/components/Landing/Landing";
 import Page from "@/components/Home/First";
+import SplashScreen from "@/components/SplashScreen/SplashScreen";
+import {useEffect,useState} from "react";
 
 export default function Home() {
+
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main className="bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200">
       
-        
-      <Page/>
+      {showSplash ? (
+        <SplashScreen />
+      ) : (
+        <Page/>
+      )}
+      
     </main>
   );
 }
---
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+const SplashScreen = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    isVisible && (
+      <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+        <img src="/logo.svg" alt="Explora logo" className="w-full h-full" />
+      </div>
+    )
+  );
+};
+
+export default SplashScreen;
