cd3001db22a5199ea00157fdc117820471d4e0de
ParagGhatage <phghatage1@gmail.com>
2024-07-21 15:45:09 +0530

    error resolved

--- src/components/Weather/CreateCityId.tsx
+++ src/components/Weather/CreateCityId.tsx
@@ -6,11 +6,12 @@ export const Id = async(Lat:number =0,Lon:number=0) => {
 
     const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY
     const CityData:any = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${Lat}&lon=${Lon}&limit=1&appid=${key}`)
-    const Cityname = CityData.data[0].name
+    console.log(CityData)
+    const Cityname = CityData?.data[0]?.name
     if(CityData){
     const data = await axios.post("/api/GetCityId",{name:Cityname})
-    
-    return data.data.id
+    console.log(data?.data?.id)
+    return data?.data?.id
 }
 else{
     return null
